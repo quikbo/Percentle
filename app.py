a965b9c44ecee1671493c6cd05eabac9746909e5
williamb301 <wboudy1@jhu.edu>
2024-04-02 21:43:26 -0400

    POST route for obtaining guess and daily_letter info working, GET route for validating guesses working

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,  render_template
+from flask import Flask,  render_template, jsonify, request
 
 app = Flask(__name__)
 
@@ -7,17 +7,23 @@ app = Flask(__name__)
 def countries():
     return render_template('countries.html')
 
-app.run(host="0.0.0.0", port=80)
-
-"""
-#sending list of valid guesses to JS file
-@app.route('/get_data')
-def compile_list_and_send_to_script():
+@app.route('/get_data', methods=['GET'])
+def GET_guesses_list():
     with open('databases/countries.txt', 'r') as f: 
         validGuessesList = f.readlines()
     data = [x.strip() for x in validGuessesList]
     return jsonify(data)
 
+@app.route('/post_data', methods=['POST'])
+def POST_guesses():
+    data_received = request.json
+    guess = data_received['guess']
+    letter = data_received['daily_letter']
+    print(guess)
+    print(letter)
+    return jsonify({'status' : 'success'})
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
-    """
+    app.run(debug=True, use_reloader=True)
+    
--- static/js/script.js
+++ static/js/script.js
@@ -10,23 +10,44 @@ document.addEventListener("DOMContentLoaded", function(){
     var header = document.getElementById("daily_letter");
     var daily_letter = getRandomChar();
     header.textContent = "Letter: " + daily_letter;
+
+    var validGuesses = []
+    fetch('/get_data') //this part works good
+        .then(response => response.json())
+        .then(data => {
+            validGuesses = data;
+        })
+        .catch(error => console.error('Error fetching data:', error));
     
 
     //function for obtaining the data from user guess submissions
     document.getElementById("countries_guess").addEventListener('submit', function(event) {
         event.preventDefault();
         var formData = new FormData(this)
-        var formDataString = JSON.stringify(Object.fromEntries(formData));
-        if ('databases/countries.txt'.includes(formData)) {
-            HandleInput(formDataString);
+        var formDataObject = Object.fromEntries(formData);
+        if (validGuesses.includes(formDataObject.guess)) {
+            HandleInput(formDataObject.guess, daily_letter);
+        } else {
+            console.log('Invalid guess, try again')
         }
     });
 });
 
 
-//handling the user's guess: see's if its a valid guess and then evaluates if its correct
-async function HandleInput(input) {
-    console.log('valid')
+//sends user's guess and letter to flask file through POST request
+function HandleInput(input, letter) {
+    const dataToSend = { guess: input, daily_letter : letter };
+
+    fetch('/post_data', {
+        method: 'POST',  // or 'PUT'
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(dataToSend),  // Convert data to JSON string
+    })
+        .then(response => response.json())  // Parse JSON response
+        .then(data => console.log(data))  // Do something with the response
+        .catch(error => console.error('Error:', error));
 }
 
 /** 
@@ -58,12 +79,6 @@ async function fetchData() {
 
 //*
     //code to obtain array list of valid guesses
-    fetch('/get-data')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);  // Process the countries list here
-            validGuesses = data;
-        })
-        .catch(error => console.error('Error fetching data:', error));
+    
     
 **/
